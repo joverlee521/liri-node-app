9f3f120eb257db25a0f9bf43529ab25aed67317f
J.Lee <joverlee521@gmail.com>
2018-10-10 11:10:54 -0700

    added movie-this command to liri

--- liri.js
+++ liri.js
@@ -1,2 +1,25 @@
-require("dotenv").config();
-var spotify = new Spotify(keys.spotify);
+var request = require("request");
+
+var command = process.argv[2];
+
+switch(command){
+    case "movie-this":
+        var movieName = "Mr. Nobody";
+        if(process.argv[3]){
+            movieName = process.argv.slice(3);
+        }
+        var queryURL = "http://www.omdbapi.com/?type=movie&t=" + movieName + "&apikey=trilogy";
+        request(queryURL, function(error, response, body){
+            if(!error && response.statusCode === 200){
+                var movie = JSON.parse(body);
+                console.log("Title: " + movie.Title);
+                console.log("Year: " + movie.Year);
+                console.log("IMDB Rating: " + movie.imdbRating);
+                console.log("Rotten Tomatoes Rating: " + movie.Ratings[1].Value);
+                console.log("Country: " + movie.Country);
+                console.log("Language: " + movie.Language);
+                console.log("Plot: " + movie.Plot);
+                console.log("Actors: " + movie.Actors);
+            }
+        })
+}
